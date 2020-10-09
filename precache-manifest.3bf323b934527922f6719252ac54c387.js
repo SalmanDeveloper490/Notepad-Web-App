64150eb73a36af8635adc04bd963ef6029ad8ec8
SalmanDeveloper490 <sk36663@gmail.com>
2020-10-09 19:53:59 +0530

    Updates

---
+++ precache-manifest.3bf323b934527922f6719252ac54c387.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa2fcd676f3956dc7a29371f81b995f7",
+    "url": "/Notepad-Web-App/index.html"
+  },
+  {
+    "revision": "83c0a82079848dee0729",
+    "url": "/Notepad-Web-App/static/css/2.19e10c4f.chunk.css"
+  },
+  {
+    "revision": "017d3226503082f751fd",
+    "url": "/Notepad-Web-App/static/css/main.29172ead.chunk.css"
+  },
+  {
+    "revision": "83c0a82079848dee0729",
+    "url": "/Notepad-Web-App/static/js/2.ea929e0d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Notepad-Web-App/static/js/2.ea929e0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "017d3226503082f751fd",
+    "url": "/Notepad-Web-App/static/js/main.393f6a9e.chunk.js"
+  },
+  {
+    "revision": "38f76c7e5fe487be1275",
+    "url": "/Notepad-Web-App/static/js/runtime-main.8b60fa50.js"
+  }
+]);
